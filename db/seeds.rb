82d1cf4a845d160ac47fe8162132d4fbd489f938
Abd El Rahman  HSN <abdelrahmanhsn1@gmail.com>
2019-03-30 06:05:35 +0200

    add votes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(first_name: "Abd El Rahman", last_name: "Hassan", email: "admin@admin.com", password: "123456789", password_confirmation: "123456789")
